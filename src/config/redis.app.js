f32cee69e4306e5ba3b755a60b0a7618819e77f3
mohamed baraka <barakamohamed946@gmail.com>
2026-04-15 18:31:02 +0200

    local redis with docker

--- src/config/redis.app.js
+++ src/config/redis.app.js
@@ -1,8 +1,16 @@
 import { Redis } from "ioredis";
 
-import { REDIS_URL } from "./env.js";
+import { REDIS_HOST, REDIS_PORT, REDIS_URL } from "./env.js";
 
-const appRedis = new Redis(REDIS_URL, {
+// Without Docker (Upstash)
+// const appRedis = new Redis(REDIS_URL, {
+//   maxRetriesPerRequest: 3,
+// });
+
+// With Docker
+const appRedis = new Redis({
+  host: REDIS_HOST,
+  port: REDIS_PORT,
   maxRetriesPerRequest: 3,
 });
 
